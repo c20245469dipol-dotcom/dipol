87a21fc99dbdc8e1e73b539bcc5184c63c682677
emmanuel0925 <c20245469.dipol@csav.edu.ph>
2026-09-04 14:41:31 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 console.log("Hello World.");
 console.log("From script.js file.");
+
+const myName ="John Emmanuel";
+let age = 60;
+const number = 9487489016;
+const address = "Napla";
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${number}`);
+console.log(`Address: ${address}`);
+console.log(67);
+
+
+
+
+
