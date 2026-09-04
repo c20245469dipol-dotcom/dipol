873c27b18e75c452238ac47e6be60b74d1a9948d
emmanuel0925 <c20245469.dipol@csav.edu.ph>
2026-09-04 16:54:34 +0800

    textContent

--- script.js
+++ script.js
@@ -22,12 +22,14 @@ function greet(greetings, names=[]){
 console.log(greet("Mayong aga", ["John Emmanuel" , "Bai", "Soy" , "Tol" , "Erp"]));
 
 const heading = document.querySelector("h1");
+heading.textContent = "Your Father's Name";
 console.log(heading);
 
-const contactHeading = document.querySelector("#contact");
+const contactHeading = document.querySelector("#contact h2");
 console.log(contactHeading);
 
 const projectHeading = document.querySelector("#services h2");
 console.log(projectHeading);
+
 
 
